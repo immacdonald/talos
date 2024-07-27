eb65e3cdb117db2d17642440df6d0a247af7535c
Ian MacDonald <ianmacdonaldm@gmail.com>
2024-07-27 12:03:23 -0400

    Altered images to be loaded once and stay in memory

--- plant.py
+++ plant.py
@@ -1,7 +1,17 @@
+from PIL import Image, ImageFile
 import pyautogui
 import time
 
-def locateOrRetry(image: str, region, count = 1):
+BREED_COMPLETE: ImageFile.ImageFile = Image.open("./images/breed_complete.png")
+PLACE_IN_NURSERY: ImageFile.ImageFile = Image.open("./images/place_in_nursery.png")
+BREED_RETRY: ImageFile.ImageFile = Image.open("./images/breed_retry.png")
+BREED: ImageFile.ImageFile = Image.open("./images/breed.png")
+NURSERY: ImageFile.ImageFile = Image.open("./images/nursery.png")
+PLANT_EGG: ImageFile.ImageFile = Image.open("./images/plant_egg.png")
+SELL_EGG: ImageFile.ImageFile = Image.open("./images/sell_egg.png")
+CONFIRM_SELL: ImageFile.ImageFile = Image.open("./images/confirm_sell.png")
+
+def locateOrRetry(image: ImageFile.ImageFile, region, count = 1):
     try:
         located_image = pyautogui.locateCenterOnScreen(image, minSearchTime=0.7, grayscale=True, confidence=0.4, region=region)
         #location_data = pyautogui.locateOnScreen(image, minSearchTime=1, grayscale=True, confidence=0.8, region=region)
@@ -10,17 +20,17 @@ def locateOrRetry(image: str, region, count = 1):
         return located_image
     except:
         if count > 3:
-            print(f'Unable to successfully find {image}, aborting program')
+            print(f'Unable to successfully find {image.filename}, aborting program')
             return None 
         else:
-            print(f'Retrying {image}')
+            print(f'Retrying {image.filename}')
             return locateOrRetry(image, region, count + 1)
 
-def interactWithImage(image: str, region = (0, 50 * 2, 1100 * 2, 800 * 2)):
+def interactWithImage(image, region = (0, 50 * 2, 1100 * 2, 800 * 2)):
     start_search = time.perf_counter()
-    location = locateOrRetry(f'images/{image}.png', region)
+    location = locateOrRetry(image, region)
     end_search = time.perf_counter()
-    print(f"Finished search for {image} in {end_search - start_search:0.2f} seconds")
+    print(f"Finished search for {image.filename} in {end_search - start_search:0.2f} seconds")
     #print(f' {image} is at {location}')
     if location is None:
         print(f'Failed to find {image}')
@@ -28,7 +38,7 @@ def interactWithImage(image: str, region = (0, 50 * 2, 1100 * 2, 800 * 2)):
     
     clickLocation(location=location)
     end_interact = time.perf_counter()
-    print(f"Finished interaction of {image} in {end_interact - end_search:0.2f} seconds")
+    print(f"Finished interaction of {image.filename} in {end_interact - end_search:0.2f} seconds")
     return location
 
 
@@ -42,29 +52,29 @@ def clickXYLocation(x, y, delay: float = 0):
 
 
 def plant_cycle():
-    breeding_cave = interactWithImage('breed_complete', region=(480, 680, 100, 80))
+    breeding_cave = interactWithImage(BREED_COMPLETE, region=(480, 680, 100, 80))
     if breeding_cave is False:
         return False
-    if interactWithImage('place_in_nursery', region=(720, 1320, 650, 100)) is False:
+    if interactWithImage(PLACE_IN_NURSERY, region=(720, 1320, 650, 100)) is False:
         return False
     clickXYLocation(x=breeding_cave.x, y=breeding_cave.y + 12, delay=0.2)
-    if interactWithImage('breed_retry', region=(400, 1400, 300, 300)) is False:
+    if interactWithImage(BREED_RETRY, region=(400, 1400, 300, 300)) is False:
         return False
-    if interactWithImage('breed', region=(830, 1300, 440, 140)) is False:
+    if interactWithImage(BREED, region=(830, 1300, 440, 140)) is False:
         return False
-    if interactWithImage('nursery', region=(1420, 850, 80, 140)) is False:
+    if interactWithImage(NURSERY, region=(1420, 850, 80, 140)) is False:
         return False
-    if interactWithImage('plant_egg', region=(40, 1430, 200, 200)) is False:
+    if interactWithImage(PLANT_EGG, region=(40, 1430, 200, 200)) is False:
         return False
-    if interactWithImage('sell_egg', region=(1280, 980, 240, 320)) is False:
+    if interactWithImage(SELL_EGG, region=(1280, 980, 240, 320)) is False:
         # Retry clicking the nursery, plant egg, and sell egg steps if needed
-        if interactWithImage('nursery') is False:
+        if interactWithImage(NURSERY) is False:
             return False
-        if interactWithImage('plant_egg', region=(40, 1430, 200, 200)) is False:
+        if interactWithImage(PLANT_EGG, region=(40, 1430, 200, 200)) is False:
             return False
-        if interactWithImage('sell_egg', region=(1280, 980, 240, 320)) is False:
+        if interactWithImage(SELL_EGG, region=(1280, 980, 240, 320)) is False:
             return False
-    if interactWithImage('confirm_sell', region=(510, 1310, 500, 130)) is False:
+    if interactWithImage(CONFIRM_SELL, region=(510, 1310, 500, 130)) is False:
         return False
     return True
 
@@ -84,10 +94,11 @@ def main():
         start_cycle = time.perf_counter()
         success = plant_cycle()
         end_cycle = time.perf_counter()
-        print(f"Finished cycle {i + 1} in {end_cycle - start_cycle:0.2f} seconds")
-        time.sleep(0.25)
-        if success == False:
-            print(f'Failed cycle {i + 1}')
+        if success: 
+            print(f"Finished cycle {i + 1} in {end_cycle - start_cycle:0.2f} seconds")
+            time.sleep(0.25)
+        elif success == False:
+            print(f'Failed cycle {i + 1} in {end_cycle - start_cycle:0.2f} seconds')
             return
     
 
